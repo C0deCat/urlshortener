f6cc6c4273fd49b5c27b00cb16d9f1b9e3ef62b6
Vladislav <vldtpk@gmail.com>
2022-07-31 12:55:11 +0300

    Added sending link to bit.ly

--- src/components/pageContent.vue
+++ src/components/pageContent.vue
@@ -2,21 +2,18 @@
 <main class="mdl-layout__content">
     <div class="page-content mdl-grid">
         <div class="mdl-cell mdl-cell--middle mdl-cell--2-offset-desktop mdl-cell--0-offset-phone  mdl-cell--8-col">
-            <UrlShortenForm></UrlShortenForm>
+            <UrlShortenForm @getLink="logEvent"></UrlShortenForm>
             <UrlList></UrlList>
         </div>
     </div>
 </main>
 </template>
 
-<script>
+<script setup>
 import UrlShortenForm from "./urlShortenForm.vue";
 import UrlList from "./urlList.vue";
 
-export default {
-    components: {
-    UrlShortenForm,
-    UrlList
+function logEvent(link) {
+    console.log('event confirmed '.concat(link));
 }
-};
 </script>
--- src/components/urlShortenForm.vue
+++ src/components/urlShortenForm.vue
@@ -1,14 +1,38 @@
 <template>
-<form action="#">
+<form @submit.prevent="getShortenLink">
   <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-    <input class="mdl-textfield__input" type="text" id="sample3">
+    <input class="mdl-textfield__input" type="text" id="sample3" v-model="link">
     <label class="mdl-textfield__label" for="sample3">URL for shortening</label>
   </div>
-  <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored">
-    Shorten
-  </button>
+  <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored">Shorten</button>
 </form>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const link = ref('');
+const emit = defineEmits(['getLink']);
+
+async function getShortenLink() {
+
+  let TOKEN = "262500e8d2ed79e0a386d1ea94a38c8c01de43dd";
+  let group_guid = "Bm7sfKTRroG";
+
+  const response = await fetch('https://api-ssl.bitly.com/v4/shorten', {
+     method: 'POST',
+     headers: {
+         'Authorization': 'Bearer '.concat(TOKEN),
+         'Content-Type': 'application/json'
+     },
+     body: JSON.stringify({ "long_url": "".concat(link.value), "domain": "bit.ly", "group_guid": "".concat(group_guid) })
+   });
+   const data = await response.json();
+
+  emit('getLink', data);
+}
+
+</script>
 
 <style scoped lang="less">
     form {
